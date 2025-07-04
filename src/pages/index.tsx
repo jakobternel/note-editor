a9080dc9d95871131ac5f25270ad9284a810e81b
Strayyyy <jakobternel@gmail.com>
2025-07-04 12:43:48 +1000

    created next.js project

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Notilo - Full-Stack Note App</title>
+                <link rel="icon" href="/icon_white.png" />
+            </Head>
+            <main>
+                <p>Next.Js App</p>
+            </main>
+        </>
+    );
+}
