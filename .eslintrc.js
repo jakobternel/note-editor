964ccb4ab1c59c4938eeb2abda5467e4cfe8ae72
Strayyyy <jakobternel@gmail.com>
2025-07-05 01:36:11 +1000

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    plugins: [
+        "@typescript-eslint",
+        "react",
+        "react-hooks",
+        "prettier",
+        "tailwindcss",
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:prettier/recommended",
+        "plugin:tailwindcss/recommended",
+    ],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "prettier/prettier": "error",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "tailwindcss/classnames-order": "warn",
+    },
+    settings: {
+        react: { version: "detect" },
+        tailwindcss: {
+            config: "./tailwind.config.js",
+        },
+    },
+};
