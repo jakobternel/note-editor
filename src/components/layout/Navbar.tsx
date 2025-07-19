8c77a99df157e7cf01647861f0308d1093b83d7d
Strayyyy <jakobternel@gmail.com>
2025-07-19 19:56:56 +1000

    added endpoint to add blank md file and navigate to it

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 import FileElement from "./navbar/FileElement";
 import NavElement from "./navbar/NavElement";
 import {
@@ -8,11 +11,18 @@ import {
     BookmarksSimpleIcon,
     TrashIcon,
 } from "@phosphor-icons/react";
+import { useToastElementStore } from "@/zustand/toastStore";
 
 /**
  * Navbar component to be used in Layout component
  */
 export default function Navbar() {
+    const router = useRouter();
+    const createToast = useToastElementStore(
+        (state) => state.createToastElement
+    );
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     return (
         <nav className="flex h-full w-48 flex-col border-r border-border bg-surface">
             <div className="flex h-16 w-full items-center gap-3 border-b border-border px-5">
@@ -30,7 +40,30 @@ export default function Navbar() {
                 <NavElement
                     navElementName="New Note"
                     icon={PlusSquareIcon}
-                    path="/new"
+                    isLoading={isLoading}
+                    clickAction={async () => {
+                        setIsLoading(true);
+                        const res = await fetch("/api/notes/create", {
+                            method: "POST",
+                        });
+
+                        const data = await res.json();
+
+                        if (data.success) {
+                            createToast(
+                                "success",
+                                "Created new note successfully."
+                            );
+                            router.push(`/note/${data.noteId}`);
+                        } else {
+                            createToast(
+                                "error",
+                                "Failed to crete new note. Please try again."
+                            );
+                        }
+
+                        setIsLoading(false);
+                    }}
                 />
                 <NavElement
                     navElementName="Dashboard"
---
+++ src/pages/api/notes/create.ts
@@ -0,0 +1,35 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from "uuid";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import s3 from "@/lib/s3";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method !== "POST") return res.status(405).end("Method not allowed");
+
+    const noteId = uuidv4(); // Generate random UUID for file name/ID
+    const fileRoute = `notes/test-user/${noteId}.md`; // File route for the generated MD file
+
+    // Set parameters for S3 file
+    const createParams = {
+        Bucket: process.env.AWS_S3_BUCKET!,
+        Key: fileRoute,
+        Body: "",
+        ContentType: "text/markdown",
+    };
+
+    try {
+        // Upload empty markdown file to S3 bucket
+        const command = new PutObjectCommand(createParams);
+        await s3.send(command);
+
+        // await s3.putObject(createParams).promise();
+
+        return res.status(200).json({ success: true, noteId, fileRoute });
+    } catch {
+        return res.status(500).json({ error: "Failed to create note" });
+    }
+}
